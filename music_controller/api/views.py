609692cddcbecbc7ad7b22ea4a8992e7b1fc9b17
anshawasthi01 <anshawasthi3894@gmail.com>
2023-01-31 18:50:43 +0530

    changes in view.py

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -2,8 +2,21 @@
 
 # # Create your views here.
 
+# ------------------------
+
+# from django.shortcuts import render
+# from django.http import HttpResponse
+
+# def main(request):
+#     return HttpResponse("Hello World")
+
+# ------------------------
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics, status
+from .models import Room 
+from .serializers import RoomSerializer
 
 def main(request):
     return HttpResponse("Hello World")
+
